d43ef8d6b69ca2b8bd190ffcb4a84a383fd4636d
Remius <banchoprod@gmail.com>
2018-06-21 18:55:45 +0200

    Initial Commit from user

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  //si je passe ma souris par dessus une musique, une croix s'affiche à droite. En cliquant dessus je supprime la chanson
+  //lorsque je veux supprimer la chanson, une requête ajax fait appel à une fonction php qui supprime en hard le fichier
+
+
+  //idem pour les playlists
+
+  //possibilité d'ajouter une playlist
+
+});
